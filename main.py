cff723cda9b4ddb2be75f42b42f4248a301372fb
209 <209@academicos.cesde.edu.co>
2022-08-05 20:07:35 -0500

    NTV1 se crea main y salida por consola

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Hola Mundo") #salida por consola
+
+#comentario de linea
+
+'''
+    comentario de bloque
+'''    
+
+#declarar variable
+
+nombre = 'Maria Fernanda'
+edad=19
+estatura=1.62
+hinchaDelMejor=True
